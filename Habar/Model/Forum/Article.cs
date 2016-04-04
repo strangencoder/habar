21f55d394906910733828e32f83867f52467ff71
Aleksandr Gorshukov <Aleksandr_Gorshukov@epam.com>
2016-04-04 09:04:18 +0000

    add model; add article, tag entities

---
+++ Habar/Model/Forum/Article.cs
@@ -0,0 +1,21 @@
+﻿using Model.Forum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public int PublisherId { get; set; }
+        public int ThemeId { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        IEnumerable<Tag> Tags { get; set; }
+        public DateTime PublishDate { get; set; }
+        public bool IsEnable { get; set; }
+    }
+}
---
+++ Habar/Model/Forum/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Forum
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
